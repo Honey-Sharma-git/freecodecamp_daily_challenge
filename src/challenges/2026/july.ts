d54b060c3aa91ceb9d817da16037151ad7161fae
Honey Sharma <honeysharmaemail@gmail.com>
2026-07-08 08:40:57 +0530

    add roundToNearestMultiple func

--- src/challenges/2026/july.ts
+++ src/challenges/2026/july.ts
@@ -1,1 +1,22 @@
-// This will contain challenge for July 2026
+/* 
+Nearest Multiple
+Given two integers, round the first to the nearest multiple of the second.
+*/
+
+function roundToNearestMultiple(num: number, multiple: number): number {
+  if (multiple === 0) return num;
+  const quotient = num / multiple;
+  const wholeNum = quotient - (quotient % 1);
+  const roundUpNum = wholeNum + 1;
+  const largerMultiple = roundUpNum * multiple;
+  const smallerMultiple = wholeNum * multiple;
+
+  const posDeviation = num - smallerMultiple;
+  const negDeviation = num - largerMultiple;
+
+  if (posDeviation < negDeviation * -1) {
+    return smallerMultiple;
+  } else {
+    return largerMultiple;
+  }
+}
